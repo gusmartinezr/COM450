81de6bc196dcc852f5b6c85c0767dbf17849ee5c
gustavomartinezrollano <gustavomartinezrollano@outlook.com>
2022-10-28 08:37:19 -0400

    prueba3 passed

--- Examen Primer Parcial/Accion/app/library/obtenerAccion.php
+++ Examen Primer Parcial/Accion/app/library/obtenerAccion.php
@@ -4,13 +4,16 @@ namespace App\Library;
 
 class obtenerAccion{
 
-    public static function obtenerAccion($esObligatorio,$esDocente){
+    public static function obtenerAccion($esObligatorio=false,$esDocente=false,$esExtremo=false,$estadoRegistro=""){
         
         if($esObligatorio==true && $esDocente==true){
             return 'actualizar';
         }
         if($esObligatorio==false && $esDocente==true){
             return 'matricular';
+        }
+        if($esExtremo==true && $estadoRegistro=="porConfirmar"){
+            return "actualizar";
         }
  }
 }
